f3fdc8c55db6959cc78e332e29d227dcfa9ba191
diegoarduca11 <71756434+diegoarduca11@users.noreply.github.com>
2021-03-31 11:39:38 +0200

    Update client.py

--- client.py
+++ client.py
@@ -14,16 +14,16 @@ protocollo = ["SYN", "SYN ACK","ACK with Data","ACK for Data"]
 step=0
 dati = str(step)
 while True:
-    try:
-        dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ")    #utente inserisce il numero di richieste
+   try:
+        dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ") #richiesta di un input 
     except EOFError:
         print("\nOkay. Exit")
         break
     if not dati:
-        print("Non puoi inviare una stringa vuota!") #controllo stringa
+        print("Non puoi inviare una stringa vuota!") #controllo
         continue
     if dati == '0':
-        print("Chiudo la connessione con il server!")
+        print("Chiudo la connessione con il server!") #chiusura del server
         break
     
     dati = dati.encode()
@@ -31,14 +31,4 @@ while True:
     sock_service.send(dati)
 
     dati = sock_service.recv(2048)
-
-    if not dati:
-        print("Server non risponde. Exit")
-        break
-    
-    dati = dati.decode()
-
-    print("Ricevuto dal server:")
-    print(dati + '\n')
-
 sock_service.close()
